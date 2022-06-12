f425dfddccddcbcb80de31c40c83e6cb079669e8
Mohammad Shahadad Hossain <shahadadrohid@gmail.com>
2022-06-12 14:58:37 +0600

    switch commit

---
+++ switch.js
@@ -0,0 +1,13 @@
+var color = 'yellow';
+switch (color) {
+    case 'green':
+        console.log('Rasta Par hoba')
+        break;
+    case 'red':
+        console.log('Stop')
+        break;
+    case 'yellow':
+        console.log('Rasta par howar jonno ready hou')
+        break;
+    default: console.log('Traffic Light')
+}
