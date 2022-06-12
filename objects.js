ab2d787d4c866412045d9fdab28ed8707d06b0ef
Mohammad Shahadad Hossain <shahadadrohid@gmail.com>
2022-06-12 14:29:20 +0600

    new commit

---
+++ objects.js
@@ -0,0 +1,32 @@
+// Declare Object 
+
+var phone = {
+    storage: '128gb',
+    price: 27000,
+    color: 'black',
+    camera: 48,
+}
+
+var computer = {
+    price: 87500,
+    storage: '1tb',
+    ram: '16gb',
+    coolingFan: 2,
+}
+computer.price = 80000;
+console.log(computer)
+
+var monitor = {
+    display: '23inch',
+    button: 5,
+    cableConnection: 2,
+    chargingCabletype: 'Threeple',
+}
+
+var book = {
+    name: 'Physics 2nd paper',
+    writer: 'Shajahan Tapan',
+    publisher: 'Hasan Book house',
+    pages: 680,
+}
+console.log(book.name)
